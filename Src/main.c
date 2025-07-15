5c62b0f81f8835dad8631e6a4641a1c54e397d7d
Yunus <pagel.yunus@gmail.com>
2025-07-15 13:01:22 +0200

    refactor: disable UART Transmit via Interrupt

--- Src/main.c
+++ Src/main.c
@@ -78,7 +78,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
   printf("counter: %ld\n", counter - last_counter);
   last_counter = counter;
   //HAL_UART_Transmit(&huart2, (uint8_t*)"Moege dein WLAN immer stark sein. Frohes neues Jahr!\n", 54, HAL_MAX_DELAY); //via Polling
-  HAL_UART_Transmit_IT(&huart2, (uint8_t*)"Moege dein WLAN immer stark sein. Frohes neues Jahr!\n", 54); //via Interrupt
+  //HAL_UART_Transmit_IT(&huart2, (uint8_t*)"Moege dein WLAN immer stark sein. Frohes neues Jahr!\n", 54); //via Interrupt
 }
 
 
